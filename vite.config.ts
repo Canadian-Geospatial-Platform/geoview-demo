6fd06d20c2bf3ecef5aed4398eb5047c515caca5
jolevesq <johann.levesque@nrcan-rncan.gc.ca>
2024-09-17 12:56:11 -0400

    core(port): Set local host port to 8080

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: "/geoview-demo/",
+  server:{
+    port: 8080,
+  },
   resolve: {
     alias: {
       '@': '/src',
